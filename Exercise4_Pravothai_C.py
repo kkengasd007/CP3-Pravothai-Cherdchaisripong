fea6214ca360791b32797cd431bae0ff5e578b67
KEGGNN <49479543+kkengasd007@users.noreply.github.com>
2019-04-27 19:11:58 +0700

    Add files via upload

---
+++ Exercise4_Pravothai_C.py
@@ -0,0 +1,13 @@
+FE = 48.5
+GB = 50.3
+ICS = 70.6
+CP = 88.8
+print("            --- Your Score ---")
+
+print("Foundation English               :", FE)
+
+print("General Business                 :", GB)
+
+print("Introduction to Computer Systems :", ICS)
+
+print("Computer Programming             :", CP)
