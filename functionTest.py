5766465405839fa3c6a2d15f44cab9f9b474e462
KEGGNN <49479543+kkengasd007@users.noreply.github.com>
2019-05-09 14:11:19 +0700

    Add files via upload

---
+++ functionTest.py
@@ -0,0 +1,16 @@
+def PlusFunction(x,y) :
+    print(x+y)
+
+def MinusFunction(x,y):
+    print(x-y)
+
+def MultiplyFunction(x,y):
+    print(x*y)
+
+def DivideFunction(x,y):
+    print(x/y)
+
+PlusFunction(10,5)
+MinusFunction(10,5)
+MultiplyFunction(10,5)
+DivideFunction(10,5)
